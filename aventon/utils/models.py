970277b9639e8b233be86c846bdf4576dcc0688d
ArnoldoRicardo <anyback@gmail.com>
2021-03-14 15:23:21 -0600

    class model abstract

---
+++ aventon/utils/models.py
@@ -0,0 +1,33 @@
+"""Django models utilities"""
+
+# Django
+from django.db import models
+
+class MetaModel(models.Model):
+    """Metadata base model
+
+    MetaModel acts as an abstract base class from which every
+    other model in the project will inherit. This class provides
+    every table with the following attributes:
+        + created (DateTime): Store the datetime the objects was created
+        + modified (DateTime): Store the last datetime the objects was modified
+    """
+
+    created = models.DateTimeField(
+        'created at',
+        auto_now_add=True,
+        help_text='Date time on which the object was created.'
+    )
+    modified = models.DateTimeField(
+        'modified at',
+        auto_now=True,
+        help_text='Date time on which the object was modified.'
+    )
+
+    class Meta:
+        """Meta option."""
+
+        abstract = True
+
+        get_lasted_by = 'created'
+        orderinng = ['-created', '-modified']
