0ce98866daa492c73640cab788f7c311572778fd
ArnoldoRicardo <anyback@gmail.com>
2021-05-03 19:39:15 -0500

    modelo de miembros

---
+++ aventon/circles/models/memberships.py
@@ -0,0 +1,47 @@
+""" Membership model. """
+rides_taken
+from django.db import models
+
+from aventon.utils.models import MetaModel
+
+class Membership(MetaModel):
+    """Membership model.
+
+    A membership is the table that holds the relationship between a user and a circle.
+    """
+
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    profile = models.ForeignKey('users.profile', on_delete=models.CASCADE)
+    circle = models.ForeignKey('circles.Circle', on_delete=models.CASCADE)
+
+    is_admin = models.BooleanField(
+        'circle admin',
+        default=False,
+        help_text="Circle admins can update ther circle's data and manage its members."
+    )
+
+    #Invitations
+    used_invitations = models.PositiveSmallIntegerField(default=0)
+    remaining_invitation = models.PositiveSmallIntegerField(default=0)
+    invited_by = models.ForeignKey(
+        'users.User',
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='invited_by'
+    )
+
+    #Stats
+    rides_taken = models.PositiveIntegerField(default=0)
+    rides_offered = models.PositiveIntegerField(default=0)
+
+    # status
+    is_active = models.BooleanField(
+        'active status',
+        default=True,
+        help_text='Only active users are allowed to interact in the circle.'
+    )
+
+    def __str__(self):
+        """Return username and circle."""
+
+        return '@{} at #{}' % (self.user.username, self.circle.slug_name)
